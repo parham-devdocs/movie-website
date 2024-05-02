4e2cd933a0635401c56055790f66c4bff568d1f5
parham-devdocs <p.77.pazargadi@gmail.com>
2024-05-02 22:06:03 +0330

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+    css: ['~/assets/css/main.css'],
+   modules: [
+    // ...
+     '@pinia/nuxt',
+     '@vueuse/nuxt',
+
+    
+  ], postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
