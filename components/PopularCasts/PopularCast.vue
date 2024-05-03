c7f7c5a4ae170fbe8b0a2ed9277df74146ad3d9e
parham-devdocs <p.77.pazargadi@gmail.com>
2024-05-03 15:15:47 +0330

    first commit

--- components/PopularCasts/PopularCast.vue
+++ components/PopularCasts/PopularCast.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="parent mt-6 bg-black relative ">
     <div class=" text-right pr-5 pb-5 text-yellow-500 font-bold">بازیگران معروف</div>
-    <div class="carousel overflow-x-hidden h-44 pt-2    " @mouseleave="up" :style="{userSelect:userSelect, scrollBehavior: scrollBehavior}" @mousedown="down"  @mouseup="up" @mousemove="move" ref="carousel">
+    <div class="carousel overflow-x-hidden h-44 pt-2    "  @mouseleave="up" :style="{userSelect:userSelect, scrollBehavior: scrollBehavior}" @mousedown="down"  @mouseup="up" @mousemove="move" ref="carousel">
      <div class="cards     " >
       
-<PopularCastsPopularCastCard v-for="(item, index) in cast" :key="index" :src="item.src" :name="item.name"></PopularCastsPopularCastCard>
+<PopularCastsPopularCastCard @mouseenter="enter" v-for="(item, index) in cast" :key="index" :src="item.src" :name="item.name"></PopularCastsPopularCastCard>
 </div> 
 <button @click="scrollLeft"  class="absolute prev text-yellow-500 text-3xl"><IconsPrevIcon></IconsPrevIcon></button>
 <button @click="scrollRight" class="  absolute next text-yellow-500 text-3xl"><IconsNextIcon></IconsNextIcon></button>
@@ -19,26 +19,26 @@
 
   import {ref } from "vue";
 const cast = ref([
-  { name: 'Yoo-in-soo', src: "../../public/photos/popularCast/1.jpg" },
-  { name: 'Wi-ha-jun', src: "../../public/photos/popularCast/2.jpg" },
-  { name: 'Peter franzen', src: "../../public/photos/popularCast/3.jpg" },
-  { name: 'Joen bae-soo', src: "../../public/photos/popularCast/4.jpg" },
-  { name: 'Alex hogh anderson', src: "../../public/photos/popularCast/5.jpg" },
-  { name: 'Jordan patrick smith', src: "../../public/photos/popularCast/6.jpg" },
-  { name: 'Marco ils', src: "../../public/photos/popularCast/7.jpg" },
-  { name: 'Danila kozlovsky', src: "../../public/photos/popularCast/8.jpg" },
-  { name: 'Georgia hirst', src: "../../public/photos/popularCast/9.jpg" },
-  { name: 'Akio otsuka', src: "../../public/photos/popularCast/10.jpg" },
-  { name: 'Lee ki-woo', src: "../../public/photos/popularCast/11.jpg" },
-  { name: 'Lee yi-kyong', src: "../../public/photos/popularCast/12.jpg" },
-  { name: 'lee ki-young', src: "../../public/photos/popularCast/13.jpg" },
-  { name: 'kensho one', src: "../../public/photos/popularCast/14.jpg" },
-  { name: 'moon ga young', src: "../../public/photos/popularCast/15.jpg" },
-  { name: 'yoto  uemora', src: "../../public/photos/popularCast/16.jpg" },
-  { name: 'yang yang', src: "../../public/photos/popularCast/17.jpg" },
-  { name: 'carman lee', src: "../../public/photos/popularCast/18.jpg" },
-  { name: 'Zhao lusi', src: "../../public/photos/popularCast/19.jpg" },
-  { name: 'Xuan lu', src: "../../public/photos/popularCast/20.jpg" },
+  { name: 'Yoo-in-soo', src: "/photos/popularCast/1.jpg" },
+  { name: 'Wi-ha-jun', src: "/photos/popularCast/2.jpg" },
+  { name: 'Peter franzen', src: "/photos/popularCast/3.jpg" },
+  { name: 'Joen bae-soo', src: "/photos/popularCast/4.jpg" },
+  { name: 'Alex hogh anderson', src: "/photos/popularCast/5.jpg" },
+  { name: 'Jordan patrick smith', src: "/photos/popularCast/6.jpg" },
+  { name: 'Marco ils', src: "/photos/popularCast/7.jpg" },
+  { name: 'Danila kozlovsky', src: "/photos/popularCast/8.jpg" },
+  { name: 'Georgia hirst', src: "/photos/popularCast/9.jpg" },
+  { name: 'Akio otsuka', src: "/photos/popularCast/10.jpg" },
+  { name: 'Lee ki-woo', src: "/photos/popularCast/11.jpg" },
+  { name: 'Lee yi-kyong', src: "/photos/popularCast/12.jpg" },
+  { name: 'lee ki-young', src: "/photos/popularCast/13.jpg" },
+  { name: 'kensho one', src: "/photos/popularCast/14.jpg" },
+  { name: 'moon ga young', src: "/photos/popularCast/15.jpg" },
+  { name: 'yoto  uemora', src: "/photos/popularCast/16.jpg" },
+  { name: 'yang yang', src: "/photos/popularCast/17.jpg" },
+  { name: 'carman lee', src: "/photos/popularCast/18.jpg" },
+  { name: 'Zhao lusi', src: "/photos/popularCast/19.jpg" },
+  { name: 'Xuan lu', src: "/photos/popularCast/20.jpg" },
 
 
 
--- components/PopularCasts/PopularCastCard.vue
+++ components/PopularCasts/PopularCastCard.vue
@@ -1,8 +1,11 @@
 <template>
-    <div class=" flex w-auto h-full flex-col items-center bg-black  ">
-    <img src="../../public/photos/popularCast/1.jpg" class=" w-32 h-32 rounded-full">
+    <a href="#" >
+    <div class=" flex w-auto h-full flex-col items-center bg-black  " >
+    <img :src="props.src" class=" w-32 h-32 rounded-full" draggable="false">
     <h2 class=" text-white mt-1">{{ name }}</h2>
     </div>
+    </a>
+
 </template>
 
 <script setup>
