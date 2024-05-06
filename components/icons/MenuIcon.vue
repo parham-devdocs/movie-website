f694f98aaafbf1487839bb8cad1453eeb406eabb
parham-devdocs <p.77.pazargadi@gmail.com>
2024-05-06 10:16:01 +0330

    first commit

---
+++ components/icons/MenuIcon.vue
@@ -0,0 +1,11 @@
+
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 24 24"><path fill="currentColor" d="M4 7a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H5a1 1 0 0 1-1-1m0 5a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H5a1 1 0 0 1-1-1m0 5a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H5a1 1 0 0 1-1-1"></path></svg>
+</template>
+
+<script>
+export default {
+  name: 'MiMenu'
+}
+</script>
